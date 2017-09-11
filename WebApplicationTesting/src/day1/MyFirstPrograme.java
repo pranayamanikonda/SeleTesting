ece860fad774b240945199b0603a1cf3499d1a03
Pranaya <pranayamanikonda@gmail.com>
2017-09-11 12:55:05 +0530

    upoad

---
+++ WebApplicationTesting/src/day1/MyFirstPrograme.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class MyFirstPrograme {
+
+	public static void main(String[] args) 
+	{
+             System.out.println("This is my programe in Java");
+             System.out.println("67775577909");
+		
+	}
+
+}
---
+++ WebApplicationTesting/src/day1/MySecondPrograme.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class MySecondPrograme {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Tis is my second programe");
+		
+
+	}
+
+}
